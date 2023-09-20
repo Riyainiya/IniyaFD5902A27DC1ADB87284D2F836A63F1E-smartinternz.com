698002b8f23e6ee32944ddfc6bc4354adf8ba0aa
Riyainiya <144340456+Riyainiya@users.noreply.github.com>
2023-09-20 12:27:53 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,12 +1,40 @@
-# implement a recursive function to calculate the factorial of a given number
+# Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality. 
 
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+   self.__account_number = account_number
+   self.__account_holder_name = account_holder_name
+   self.__account_balance = initial_balance
+  def deposit(self,amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. new balance: ₹{}".format(amount,
+                                                     self.__account_balance))
+    else:
+      print("Invalid deposit amount. please deposit a positive amount.")
 
-number=2
-res=fact_rec(number)
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("withdraw ₹{}. New balance: ₹{}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+      
+  def display_balance(self):
+    print("Account balance for {} (Account ₹{}): ₹{}".format(
+    self.__account_holder_name, self.__account_number,
+    self.__account_balance))
 
-print (" the factorial of {} is {}.". format (number ,res ))
+
+account = BankAccount(account_number="123456789",
+                      account_holder_name="riya",
+                      initial_balance=5000.0)
+
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
+
+    
